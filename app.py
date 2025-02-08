9d84a9d23c661dba2466a9e2c7543b7d32296c9a
emad-mohamadi <seyedemadmohammadi@gmail.com>
2025-02-08 21:45:31 +0330

    search test

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,12 @@ app = Flask(__name__)
 @app.route('/hello')
 def hello():
     return jsonify({"result": "Hello"})
+
+
+@app.route('/search')
+def search():
+    query = request.args.get('q')
+    return jsonify({"result": f"You searched {query}"})
 
 
 if __name__ == '__main__':
