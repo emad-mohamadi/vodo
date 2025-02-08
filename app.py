b1ffb191b00bce4d82744cfff5d7912c448b98b0
emad-mohamadi <seyedemadmohammadi@gmail.com>
2025-02-08 19:29:15 +0330

    test local flask app

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/hello')
+def hello():
+    return jsonify({"result": "Hello"})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
