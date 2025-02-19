fc3a745d7fcccf4b219d244f8d50e63683190481
emad-mohamadi <seyedemadmohammadi@gmail.com>
2025-02-19 10:34:47 +0330

    task functions added to logic

--- logic.py
+++ logic.py
@@ -1,33 +1,64 @@
-from datetime import datetime
+from datetime import datetime as dt
 from supabase import Client
 from os import environ
 
-# key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5mbm1jaGlnendoeWV2Y3h6Z3d5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg5Mjg0ODEsImV4cCI6MjA1NDUwNDQ4MX0.82X66JQi3AaRNdzW8Zhg_0v5RKgElCAk8HXEym2gm5k"
-# url = "https://nfnmchigzwhyevcxzgwy.supabase.co"
+
+class Task:
+    def __init__(self, name, description=None, category=None, datetime=None, repeat=None, duration=None):
+        self.name = name
+        self.description = description
+        self.category = category
+        self.completed = False
+        self.tags = []
+        self.datetime = datetime
+        self.repeat = repeat
+        self.duration = duration
+        self.comment = None
+        self.feedback = None
+        self.created_at = dt.now().__str__()
+
+    def __repr__(self):
+        return {
+            "created_at": self.created_at,
+            "name": self.name,
+            "description": self.description,
+            "category": self.category,
+            "completed": self.completed,
+            "tags": self.tags,
+            "duration": self.duration,
+            "datetime": self.datetime,
+            "repeat": self.repeat,
+            "comment": self.comment,
+            "feedback": self.feedback,
+        }
 
 
 class DataBase(Client):
     def __init__(self, url=environ.get("SUPABASE_URL"), key=environ.get("SUPABASE_KEY")):
         super().__init__(url, key)
 
-    def get_tasks(self):
-        response = self.table('tasks').select('*').execute()
-        return response.data
+    def add_task(self, task: Task, id: int):
+        response = self.table("data").select("tasks").eq("id", id).execute()
+        self.table("data").update(
+            {
+                "tasks":
+                    response.data[0]["tasks"] + [task.__repr__()],
+            }
+        ).eq("id", id).execute()
+        return
 
-
-class Task:
-    def __init__(self, id, name, description="", category="uncategorized"):
-        self.id = id
-        self.name = name
-        self.description = description
-        self.category = category
-        self.completed = False
-        self.created_at = datetime.now()
-
-    def complete_toggle(self):
-        self.completed = not self.completed
+    def modify_task(self, task: Task, index: int, id: int):
+        response = self.table("data").select("tasks").eq("id", id).execute()
+        response.data[0]["tasks"][index] = task.__repr__()
+        self.table("data").update(
+            {
+                "tasks":
+                    response.data[0]["tasks"],
+            }
+        ).eq("id", id).execute()
         return
 
 
-data = DataBase()
-print(data.get_tasks())
+# task = Task(name="Reading a book")
+# data = DataBase()
+# data.add_task(task, 1)
