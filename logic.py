12c1b85e234ee18ae739e081490660db48466fa2
danial-fazel <danialfzl832@gmail.com>
2025-03-06 10:28:12 +0330

    repeat added

--- logic.py
+++ logic.py
@@ -1,8 +1,8 @@
 from supabase import Client
 from os import environ
 from uuid import uuid1
-from datetime import datetime
-from datetime import timezone
+from datetime import datetime, timedelta, timezone
+from dateutil.relativedelta import relativedelta  
 
 class DataBase(Client):
     def __init__(self, url=environ.get("SUPABASE_URL"), key=environ.get("SUPABASE_KEY")):
@@ -30,6 +30,7 @@ class DataBase(Client):
         ).eq("uuid", uuid).execute()
         return
 
+
     def get_tasks(self, user_id):
         task_ids_response = self.table("users").select("tasks").eq("id", user_id).execute()
         task_ids = task_ids_response.data[0]["tasks"] if task_ids_response.data else []
@@ -41,8 +42,32 @@ class DataBase(Client):
         today_start = now.replace(hour=0, minute=0, second=0, microsecond=0)
         today_end = now.replace(hour=23, minute=59, second=59, microsecond=999999)
 
-        ongoing_tasks = {task["uuid"]: task for task in tasks if today_end < datetime.fromisoformat(task["datetime"])}
-        today_tasks = {task["uuid"]: task for task in tasks if today_start <= datetime.fromisoformat(task["datetime"]) <= today_end}
-        overdue_tasks = {task["uuid"]: task for task in tasks if today_start > datetime.fromisoformat(task["datetime"])}
+        ongoing_tasks = {}
+        today_tasks = {}
+        overdue_tasks = {}
+
+        for task in tasks:
+            task_datetime = task["datetime"]
+            repeat = task.get("repeat", "none")  # Default is "none" if missing
+
+            # Handle repeating tasks
+            if repeat != "none":
+                while task_datetime < today_start:
+                    if repeat == "daily":
+                        task_datetime += timedelta(days=1)
+                    elif repeat == "weekly":
+                        task_datetime += timedelta(weeks=1)
+                    elif repeat == "monthly":
+                        task_datetime += relativedelta(months=1)
+                    elif repeat == "yearly":
+                        task_datetime += relativedelta(years=1)
+
+            # Categorize tasks
+            if task_datetime > today_end:
+                ongoing_tasks[task["uuid"]] = task
+            elif today_start <= task_datetime <= today_end:
+                today_tasks[task["uuid"]] = task
+            else:
+                overdue_tasks[task["uuid"]] = task
 
         return ongoing_tasks, today_tasks, overdue_tasks
